90dfa0885de6996bcd00e64a30804fd99d23dcbe
Andrew Smith <andrewcs149@gmail.com>
2020-07-06 14:27:20 -0700

    Create an array from the user input.

--- Numbers-Game/Numbers-Game/Program.cs
+++ Numbers-Game/Numbers-Game/Program.cs
@@ -12,10 +12,19 @@ namespace Numbers_Game
 
         static void StartSequence()
         {
+            // Request int from user and store in a variable
             Console.Write("Please enter a number greather than zero: ");
             int num = Convert.ToInt32(Console.ReadLine());
 
+            // Create an arr the length of the user input
+            int[] numArr = new int[num];
 
+            // Begin Sequence
+            //GetSum(numArr);
+
+            //GetProduct();
+
+            //GetQuotient();
         }
     }
 }
